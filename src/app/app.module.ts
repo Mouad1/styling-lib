dec9d3515f03631034f8b05af84f5b3621ec6c87
Mouad Belghiti <mouad.belghiti.ext@asn.com>
2024-02-07 18:31:25 +0100

    creating style base , buttons and typos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
+import { ButtonShowcaseComponent } from './button-showcase/button-showcase.component';
+import { TypographyComponent } from './typography/typography.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ButtonShowcaseComponent, TypographyComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideAnimations()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/styles/styles.md
@@ -0,0 +1,6 @@
+// This file contains the descriptions of the functionning of the application scss files
+the kraken styling library sturcutres still the same we have a styles folder and inside it we find 4 sub-folder :
+-base : \_colors, \_topography, \_palletes and \_variables
+-components : the atomic components (buttons,cards...)
+-theme:angular theme configuration for the kraken app
+utils : the functions and mixings
